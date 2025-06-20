be7cd353cce082d4bb80ac66c28aa9e48e92ad1d
anastasiia <anastasiia.shevtsova@right.link>
2025-06-20 19:03:20 +0200

    Implement user schema and model. Add mongoose validations. Implement password encrypting.

---
+++ models/User.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+import isEmail from 'validator/lib/isEmail.js'
+import bcrypt from 'bcrypt'
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      validate: [isEmail, 'Please enter a valid email'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [6, 'Minimum password length is 6 characters'],
+    },
+  },
+  { timestamps: true, versionKey: false }
+)
+
+userSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt()
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
+})
+
+const User = mongoose.model('User', userSchema)
+
+export default User
