471d138f25852be4cf2a6aa332dc171e927e686b
anastasiia <anastasiia.shevtsova@right.link>
2025-06-21 15:12:59 +0200

    Update user model. Implement login method. Update auth routes.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { register, login } from '../controllers/authController.js'
+import { register, login, logout, checkUser } from '../controllers/authController.js'
 
 const router = express.Router()
 
 router.post('/register', register)
 router.post('/login', login)
+router.get('/logout', logout)
+router.get('/user', checkUser)
 
 export default router
