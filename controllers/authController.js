7015772fa9343bce476c08b7331f8a1350a4f749
anastasiia <anastasiia.shevtsova@right.link>
2025-06-21 15:13:58 +0200

    Update auth controller. Implement login, logout and user endpoints logic. Update error handling.

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,6 +7,14 @@ dotenv.config()
 const handleErrors = (error) => {
   console.log(error.message, error.code)
   let errors = { email: '', password: '' }
+
+  if (error.message.includes('incorrect email')) {
+    errors.email = 'Email is not registered'
+  }
+  if (error.message.includes('incorrect password')) {
+    errors.password = 'Password is incorrect'
+  }
+
   if (error.message.includes('User validation failed')) {
     Object.values(error.errors).forEach(({ properties }) => {
       errors[properties.path] = properties.message
@@ -44,15 +52,44 @@ export const register = async (req, res) => {
 }
 
 export const login = async (req, res) => {
+  const { email, password } = req.body
   try {
-    const { email, password } = req.body
+    const user = await User.login(email, password)
+    const token = createToken(user._id)
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
 
-    const user = await User.findOne({ email })
-    if (!user) {
-      return res.status(400).json({ message: 'User not found' })
-    }
+    res.status(200).json({ user: user._id })
   } catch (error) {
     const errors = handleErrors(error)
     res.status(400).json({ errors })
+  }
+}
+
+export const logout = (req, res) => {
+  res.cookie('jwt', '', { maxAge: 1 })
+  res.status(200).json({ message: 'User logged out' })
+}
+
+export const checkUser = async (req, res) => {
+  const token = req.cookies.jwt
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
+      if (err) {
+        return res.status(401).json({ user: null, message: 'Invalid token' })
+      } else {
+        try {
+          const user = await User.findById(decodedToken.id).select('-password')
+          if (user) {
+            return res.status(200).json({ user })
+          } else {
+            return res.status(404).json({ user: null, message: 'User not found' })
+          }
+        } catch (error) {
+          return res.status(500).json({ user: null, message: 'Internal server error' })
+        }
+      }
+    })
+  } else {
+    return res.status(401).json({ user: null, message: 'No token provided' })
   }
 }
