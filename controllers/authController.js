7627e303a7000c8a655d77e458817d6f5ab66320
anastasiia <anastasiia.shevtsova@right.link>
2025-06-21 17:24:16 +0200

    Update logout cookie configuration.

--- controllers/authController.js
+++ controllers/authController.js
@@ -72,7 +72,12 @@ export const login = async (req, res) => {
 }
 
 export const logout = (req, res) => {
-  res.cookie('jwt', '', { maxAge: 1 })
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    maxAge: 1,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+  })
   res.status(200).json({ message: 'User logged out' })
 }
 
